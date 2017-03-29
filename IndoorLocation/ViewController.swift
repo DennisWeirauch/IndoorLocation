15421392a1994bc2bf6a3f2f080178a808f5b975
Dennis Hirschgänger <d.hirschgaenger@web.de>
2017-03-29 11:28:43 +0200

    Initial Commit

---
+++ IndoorLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IndoorLocation
+//
+//  Created by Dennis Hirschgänger on 29/03/2017.
+//  Copyright © 2017 Hirschgaenger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
