87ce8a8f284c4084df7ea6f6e1334591a88cec62
Dennis Hirschgänger <d.hirschgaenger@web.de>
2017-05-11 13:10:40 +0200

    Added Embassy as Server on iOS

---
+++ Podfile
@@ -0,0 +1,7 @@
+project 'IndoorLocation.xcodeproj'
+platform :ios, '10.2'
+use_frameworks!
+
+target 'IndoorLocation' do
+		pod 'Embassy'
+end
---
+++ Pods/Target Support Files/Pods-IndoorLocation/Pods-IndoorLocation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IndoorLocation : NSObject
+@end
+@implementation PodsDummy_Pods_IndoorLocation
+@end
